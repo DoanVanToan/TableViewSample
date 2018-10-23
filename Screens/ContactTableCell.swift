502d272d7f4ac63fd25470c434a88da3272116a5
doan.van.toan <toandoan.coder@gmail.com>
2018-10-23 16:28:59 +0700

    init table view

---
+++ Screens/ContactTableCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContactTableCell.swift
+//  TableViewSample
+//
+//  Created by doan.van.toan on 10/23/18.
+//  Copyright © 2018 doan.van.toan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactTableCell: UITableViewCell {
+    
+}
