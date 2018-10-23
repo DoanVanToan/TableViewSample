725cbee0558659e5666e7b70ad76a700183d437a
doan.van.toan <toandoan.coder@gmail.com>
2018-10-23 11:22:08 +0700

    init project

---
+++ AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TableViewSample
+//
+//  Created by doan.van.toan on 10/23/18.
+//  Copyright © 2018 doan.van.toan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
